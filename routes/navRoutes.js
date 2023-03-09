d558ebe0f75ac295a0bac21ea24c216deb8c085c
thelouce <100891923+thelouce@users.noreply.github.com>
2023-03-08 19:23:00 -0500

    massive update
    routing, a database, all the good stuff

---
+++ routes/navRoutes.js
@@ -0,0 +1,22 @@
+const { Router } = require('express');
+const navController = require('../controllers/navControllers')
+
+//setting up a router
+const router = Router();
+
+router.get('/', navController.courses_index);
+
+router.get('/instructor', navController.instructor_index);
+router.post('/instructor', navController.instructor_post);
+
+router.get('/login', navController.login_index);
+router.post('/login', navController.login_post);
+
+router.get('/logout', navController.logout_index);
+
+router.get('/student', navController.student_index);
+
+router.get('/signup', navController.signup_index);
+router.post('/signup', navController.signup_post);
+
+module.exports = router;
